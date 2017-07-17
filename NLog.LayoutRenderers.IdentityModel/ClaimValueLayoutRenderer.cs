03c4fa2f717ea3b9cb1dc6071290364907b79740
Mateusz Turzyński <mateusz.turzynski@goyello.com>
2017-07-17 09:14:14 +0200

    initial commit

---
+++ NLog.LayoutRenderers.IdentityModel/ClaimValueLayoutRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using NLog.Config;
+
+namespace NLog.LayoutRenderers.IdentityModel
+{
+    [LayoutRenderer("claim-value")]
+    public class ClaimValueLayoutRenderer : LayoutRenderer
+    {
+        [RequiredParameter]
+        [DefaultParameter]
+        public string TypeName { get; set; }
+
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var claimValue = ClaimsPrincipal.Current?.Claims.FirstOrDefault(x => x.Type == TypeName)?.Value ?? string.Empty;
+            builder.Append(claimValue);
+        }
+    }
+}
